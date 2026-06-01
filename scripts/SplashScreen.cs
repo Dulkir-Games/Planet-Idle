4a7ee26bfc27100403c3b5c83feab0885cf5bee9
Ronald Inglett <71849533+inglettronald@users.noreply.github.com>
2026-05-31 19:18:04 -0500

    fix: lint error

--- scripts/SplashScreen.cs
+++ scripts/SplashScreen.cs
@@ -1,28 +1,27 @@
-namespace PlanetIdle.scripts;
-
 using Godot;
-using System;
+
+namespace PlanetIdle.scripts;
 
 public partial class SplashScreen : Control {
 
-	private AnimationPlayer _animationPlayer;
-	private Control _mainMenu;
+    private AnimationPlayer _animationPlayer;
+    private Control _mainMenu;
 
-	public override void _Ready() {
+    public override void _Ready() {
         this._animationPlayer = this.GetNode<AnimationPlayer>("AnimationPlayer");
         this._mainMenu = this.GetNode<Control>("MainMenu");
-        
-		this._animationPlayer.Play("MenuFadeIn");
-	}
+
+        this._animationPlayer.Play("MenuFadeIn");
+    }
 
     public override void _Input(InputEvent @event) {
         base._Input(@event);
-		if (!@event.IsPressed()) {
-			return;
-		}
+        if (!@event.IsPressed()) {
+            return;
+        }
 
         this._animationPlayer.Stop();
-		this._mainMenu.Modulate = Colors.White;
+        this._mainMenu.Modulate = Colors.White;
     }
 
 }
